3ac3c5bdb1c3a76e715f908b8e9ec624b72381ef
Rafael Araujo <rafa123839@gmail.com>
2020-12-31 19:10:29 -0300

    layout academia

---
+++ script.js
@@ -0,0 +1,56 @@
+var toggle = document.querySelector('.toggle');
+var menuLista = document.querySelector('.menu-toggle')
+var btn = document.querySelectorAll('.btn');
+
+toggle.addEventListener('click', function(){
+
+    menuLista.classList.toggle('show');
+})
+
+function scroolSuave(){
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href)
+        const topo = section.offsetTop;
+    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth'
+        // })
+    }
+    
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    });
+}
+
+scroolSuave();
+
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScrool(){
+            sections.forEach( (section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                const isSectionVisible = sectionTop - windowMetade;
+                if(sectionTop < 0 ){
+                    section.classList.add('ativo')
+                }
+            })
+        }
+        animaScrool()
+        window.addEventListener('scroll', animaScrool)
+    }
+}
+
+initAnimacaoScroll()
